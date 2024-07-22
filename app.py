38975397b8405beabc009714783dc839d5f4e52c
sai krishna <sai.jgs@gmail.com>
2024-07-22 11:41:56 -0400

    version update

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pickle
 import numpy as np
-from sklearn.preprocessing import MinMaxScaler
+
 
 # Load the saved model and scaler
 with open('Randomforest_model.pkl', 'rb') as file:
