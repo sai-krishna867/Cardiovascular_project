d0bedd8bb375bedc3c19f3688328070d590b2e91
sai krishna <sai.jgs@gmail.com>
2024-07-22 11:12:56 -0400

    deployement update

--- app.py
+++ app.py
@@ -6,8 +6,7 @@ from sklearn.preprocessing import MinMaxScaler
 # Load the saved model and scaler
 with open('Randomforest_model.pkl', 'rb') as file:
     model = pickle.load(file)
-with open('scaler.pkl', 'rb') as file:
-    scaler = pickle.load(file)
+
 # Load the scaler (Assuming you've saved and loaded it)
 # If the scaler was not saved, you need to fit it on the training data used for the model
 # and save it similarly as the model.
@@ -17,8 +16,9 @@ with open('scaler.pkl', 'rb') as file:
 
 # Function to take user input and make prediction
 def predict_chd(input_data):
-    input_data_scaled = scaler.transform([input_data])
-    prediction = model.predict(input_data_scaled)
+    #input_data_scaled = scaler.transform([input_data])
+    
+    prediction = model.predict(np.array(input_data).reshape(1, -1))
     return prediction
 
 
